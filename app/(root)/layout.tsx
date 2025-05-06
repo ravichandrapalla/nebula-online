75240fad079a550024e31d13f92366b46d434d11
ravichandrapalla <cravi6635@gmail.com>
2025-05-06 07:06:47 +0530

    added header and footer for rootlayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/assets/styles/globals.css";
 import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <div className="flex flex-col h-screen">
       <Header />
       <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
-    return (<div>Header</div> );
-}
- 
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              alt="iamge-logo"
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden font-bold text-2xl ml-3 lg:block">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <UserIcon /> Sign-in
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
